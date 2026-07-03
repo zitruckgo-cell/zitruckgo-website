1260c681a3d32e61bfb68a1355c322e0c8f2f1bf
zitruckgo-cell <zitruckgo@gmail.com>
2026-07-03 05:58:59 +0000

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,37 @@
+/** @type {import('tailwindcss').Config} */
+
+export default {
+  
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  
+  theme: {
+    
+    extend: {
+      
+      colors: {
+        
+        primary: "#5a4628",
+        
+        secondary: "#3a8d8d",
+        
+      },
+      
+    },
+    
+  },
+  
+  plugins: [],
+  
+};
+
+
+
+
+
+
+
+
+
+
+
+
