5257b5c1bafc7f9f52b81b6aab4edb86ec5ca890
zitruckgo-cell <zitruckgo@gmail.com>
2026-07-03 06:14:18 +0000

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,37 +1,11 @@
 /** @type {import('tailwindcss').Config} */
-
-export default {
-  
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
-  
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
-    
-    extend: {
-      
-      colors: {
-        
-        primary: "#5a4628",
-        
-        secondary: "#3a8d8d",
-        
-      },
-      
-    },
-    
+    extend: {},
   },
-  
   plugins: [],
-  
-};
-
-
-
-
-
-
-
-
-
-
-
-
+}
